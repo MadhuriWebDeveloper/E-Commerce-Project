1fa224b5dac649f297ad47b2e76a2ced54b7b292
MadhuriWebDeveloper <145983285+MadhuriWebDeveloper@users.noreply.github.com>
2023-10-20 09:51:26 -0700

    Add files via upload
    E-commerce project

---
+++ all_product.js
@@ -0,0 +1,39 @@
+import product1 from "./product1.png";
+import product2 from "./product2.png";
+import product3 from "./product3.png";
+import product4 from "./product4.png";
+
+let all_product = [
+    {
+        id:1,
+        name:"product1",
+        category:"women",
+        image:product1,
+        new_price:50.0,
+        old_price:80.5,
+    },
+    {
+        id:2,
+        name:"product2",
+        category:"women",
+        image:product2,
+        new_price:60.0,
+        old_price:90.5,
+    },
+    {
+        id:3,
+        name:"product3",
+        category:"men",
+        image:product3,
+        new_price:150.0,
+        old_price:180.5,
+    },
+    {
+        id:4,
+        name:"product4",
+        category:"men",
+        image:product4,
+        new_price:110.0,
+        old_price:150.5,
+    },
+];
